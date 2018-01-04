addee195715c99a8ce9501fb9d914d07870681f8
Gianluca Nitti <gianluca.nitti96@gmail.com>
2018-01-04 18:36:51 +0100

    Parallel over IP

--- printserver/printserver.ino
+++ printserver/printserver.ino
@@ -1,56 +1,38 @@
-#define STROBE 10
-#define BUSY 9
-#define STROBE_DELAY 20
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
 
-const int dataPins[8] = {D0, D1, D2, D3, D4, D5, D6, D7};
-
+#define TCP_SERVER_PORT 12345
+WiFiServer tcpServer(TCP_SERVER_PORT);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("pin setup...");
-  pinMode(BUSY, INPUT);
-  pinMode(STROBE, OUTPUT);
-  digitalWrite(STROBE, HIGH);
-  for (int i = 0; i < 8; i++) {
-    pinMode(dataPins[i], OUTPUT);
-    digitalWrite(dataPins[i], LOW);
-  }
-  Serial.println("pin setup ok");
-  Serial.println("ready!");
-  /*printByte(27);
-  printByte('E');
-  printByte('A');
-  printByte(27);
-  printByte('E');*/
-}
-
-void waitForPrinterReady() {
-  //Serial.println("waiting...");
-  while (digitalRead(BUSY) == HIGH) {
-    yield();
-  }
-  //Serial.println("ready to print byte");
-}
-
-void printByte(uint8_t data) {
-  waitForPrinterReady();
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(dataPins[i], bitRead(data, i));
-  }
-  digitalWrite(STROBE, LOW);
-  delayMicroseconds(STROBE_DELAY);
-  digitalWrite(STROBE, HIGH);
-  waitForPrinterReady();
+  Serial.println("boot ok");
+  parallelPort_setup();
+  wifi_setup();
+  wifi_waitOnline();
+  tcpServer.begin();
+  Serial.println("setup ok");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-      uint8_t data = Serial.read();
-
-      Serial.write(data);
-      printByte(data);
-      Serial.write(data);
+  Serial.println(wifi_info());
+  WiFiClient c = tcpServer.available();
+  if (c) {
+    Serial.println("---client " + c.remoteIP().toString() + ":" + c.remotePort() + " connected");
+    printDataFromClient(c);
+    c.stop();
+    Serial.println("---connection closed---");
   }
-  delay(500);
+  delay(1000);
+}
+
+void printDataFromClient(WiFiClient c) {
+  while (c.connected()) {
+    if (c.available()) {
+      byte b = c.read();
+      Serial.write(b);
+      parallelPort_printByte(b);
+    }
+  }
 }
---
+++ printserver/wifi.ino
@@ -0,0 +1,20 @@
+#define DEFAULT_WIFI_SSID "il-mio-ssid"
+#define DEFAULT_WIFI_PASS "password"
+
+void wifi_setup() {
+  WiFi.begin(DEFAULT_WIFI_SSID, DEFAULT_WIFI_PASS);
+}
+
+void wifi_waitOnline() {
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(200);
+  }
+}
+
+String wifi_info() {
+  if (WiFi.status() == WL_CONNECTED) {
+    return "Online, IP = " + WiFi.localIP().toString() + ", signal = " + WiFi.RSSI() + "dBm";
+  } else {
+    return "Offline";
+  }
+}

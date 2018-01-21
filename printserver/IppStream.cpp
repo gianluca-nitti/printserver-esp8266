4f6b137e693a00e9acb1dfd837250fad03972248
Gianluca Nitti <gianluca.nitti96@gmail.com>
2018-01-21 17:41:58 +0100

    Improve responses to IPP Get-Printer-Attributes

--- printserver/IppStream.cpp
+++ printserver/IppStream.cpp
@@ -1,5 +1,27 @@
 #include "IppStream.h"
 
+std::set<String> allPrinterDescriptionAttributes {
+  "charset-configured",
+  "charset-supported",
+  "compression-supported",
+  "document-format-default",
+  "document-format-supported",
+  "generated-natural-language-supported",
+  "ipp-versions-supported",
+  "natural-language-configured",
+  "operations-supported",
+  "pdl-override-supported",
+  "printer-name",
+  "printer-is-accepting-jobs",
+  "printer-state",
+  "printer-state-reasons",
+  "printer-up-time",
+  "printer-uri-supported",
+  "queued-job-count",
+  "uri-authentication-supported",
+  "uri-security-supported"
+};
+
 IppStream::IppStream(WiFiClient conn): HttpStream(conn) {
 }
 
@@ -8,6 +30,7 @@ std::map<String, std::set<String>> IppStream::parseRequestAttributes() {
   byte tag = read();
   if (tag == IPP_OPERATION_ATTRIBUTES_TAG) {
     String name = "";
+    int index = 0;
     while ((tag = read()) >= 0x10) { //if tag >= 0x10, then it's a value-tag
       uint16_t nameLength = read2Bytes();
       if (nameLength != 0) {
@@ -15,8 +38,13 @@ std::map<String, std::set<String>> IppStream::parseRequestAttributes() {
       } //otherwise, it's another value for the previous attribute
       uint16_t valueLength = read2Bytes();
       String value = readString(valueLength);
+      if ((index == 0 && name != "attributes-charset") || (index == 1 && name != "attributes-natural-language")) {
+        result.clear();
+        return result;
+      }
       result[name].insert(value);
-      //Serial.printf("Parsed IPP attribute: tag=0x%02X, name=\"%s\", value=\"%s\"\r\n", tag, name.c_str(), value.c_str());
+      index++;
+      Serial.printf("Parsed IPP attribute: tag=0x%02X, name=\"%s\", value=\"%s\"\r\n", tag, name.c_str(), value.c_str());
     }
   } else if (tag != IPP_END_OF_ATTRIBUTES_TAG) {
     //bad request, TODO throw
@@ -24,11 +52,14 @@ std::map<String, std::set<String>> IppStream::parseRequestAttributes() {
   return result;
 }
 
-void IppStream::beginResponse(uint16_t statusCode, uint32_t requestId) {
-  print("HTTP/1.1 100 Continue\r\n\r\nHTTP/1.1 200 OK\r\n\r\n");
+void IppStream::beginResponse(uint16_t statusCode, uint32_t requestId, String charset) {
+  print("HTTP/1.1 100 Continue\r\n\r\nHTTP/1.1 200 OK\r\nContent-Type: application/ipp\r\n\r\n");
   write2Bytes(IPP_SUPPORTED_VERSION);
   write2Bytes(statusCode);
   write4Bytes(requestId);
+  write(IPP_OPERATION_ATTRIBUTES_TAG);
+  writeAttribute(IPP_VALUE_TAG_CHARSET, "attributes-charset", charset);
+  writeAttribute(IPP_VALUE_TAG_NATURAL_LANGUAGE, "attributes-natural-language", "en-us");
 }
 
 void IppStream::writeAttribute(byte valueTag, String name, String value) {
@@ -40,11 +71,85 @@ void IppStream::writeAttribute(byte valueTag, String name, String value) {
 }
 
 void IppStream::writeAttribute(String name, attribute_value_t value) {
-  writeAttribute(value.valueTag, name, value.value);
+  write(value.valueTag);
+  write2Bytes((uint16_t) name.length());
+  print(name);
+  write2Bytes((uint16_t) value.valueLength);
+  for (int i = 0; i < value.valueLength; i++) {
+    write(value.value[i]);
+  }
 }
 
 attribute_value_t getPrinterAttribute(String name) {
-  return {IPP_VALUE_TAG_UNSUPPORTED, ""};
+  if (name == "charset-configured") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_CHARSET, "utf-8");
+  } else if (name == "charset-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_CHARSET, "utf-8");
+  } else if (name == "compression-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_KEYWORD, "none");
+  } else if (name == "document-format-default") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_MIME_MEDIA_TYPE, "text/plain");
+  } else if (name == "document-format-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_MIME_MEDIA_TYPE, "text/plain");
+  } else if (name == "generated-natural-language-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_NATURAL_LANGUAGE, "en-us");
+  } else if (name =="ipp-versions-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_KEYWORD, "1.1");
+  } else if (name == "natural-language-configured") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_NATURAL_LANGUAGE, "en-us");
+  } else if (name == "operations-supported") {
+    return {IPP_VALUE_TAG_ENUM, "\0\0\0\x02", 4};
+  } else if (name == "pdl-override-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_KEYWORD, "en-us");
+  } else if (name == "printer-name") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_NAME, "ESP8266 print server"); //TODO
+  } else if (name =="printer-is-accepting-jobs") {
+    return {IPP_VALUE_TAG_BOOLEAN, "\x01", 1};
+  } else if (name == "printer-state") {
+    return {IPP_VALUE_TAG_ENUM, "\0\0\0\x03", 4}; //3 = idle
+  } else if (name == "printer-state-reasons") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_KEYWORD, "none");
+  } else if (name == "printer-up-time") {
+    return {IPP_VALUE_TAG_INTEGER, "\0\0\0\x01", 4};
+  } else if (name == "printer-uri-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_URI, "ipp://192.168.1.1"); //TODO
+  } else if (name == "queued-job-count") {
+    return {IPP_VALUE_TAG_INTEGER, "\0\0\0\0", 4};
+  } else if (name == "uri-authentication-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_KEYWORD, "none");
+  } else if (name == "uri-security-supported") {
+    return STRING_ATTRIBUTE_VALUE(IPP_VALUE_TAG_KEYWORD, "none");
+  } else {
+    return {IPP_VALUE_TAG_UNSUPPORTED, ""};
+  }
+}
+
+void IppStream::handleGetPrinterAttributesRequest(std::map<String, std::set<String>> requestAttributes) {
+  std::set<String>& requestedAttributes = requestAttributes["requested-attributes"];
+  std::set<String> supportedAttributes;
+
+  if (requestedAttributes.size() == 0 || (requestedAttributes.find("all") != requestedAttributes.end()) || (requestedAttributes.find("printer-description") != requestedAttributes.end())) {
+    Serial.println("----sending all attributes");
+    requestedAttributes = allPrinterDescriptionAttributes;
+  }
+
+  write(IPP_UNSUPPORTED_ATTRIBUTES_TAG);
+  for (String attributeName: requestedAttributes) {
+    if (getPrinterAttribute(attributeName).valueTag == IPP_VALUE_TAG_UNSUPPORTED) {
+      writeAttribute(IPP_VALUE_TAG_UNSUPPORTED, attributeName, "unsupported");
+      Serial.printf("----------------------------Unsupported attribute: \"%s\"\r\n", attributeName.c_str());
+    } else {
+      supportedAttributes.insert(attributeName);
+    }
+  }
+
+  write(IPP_PRINTER_ATTRIBUTES_TAG);
+  for (String attributeName: supportedAttributes) {
+    Serial.printf("------------------------------Supported attribute: \"%s\"\r\n", attributeName.c_str());
+    writeAttribute(attributeName, getPrinterAttribute(attributeName));
+  }
+
+  write(IPP_END_OF_ATTRIBUTES_TAG);
 }
 
 bool IppStream::parseRequest() {
@@ -60,45 +165,45 @@ bool IppStream::parseRequest() {
   uint16_t operationId = read2Bytes();
   uint32_t requestId = read4Bytes();
   Serial.printf("Received IPP request; Version: 0x%04X, OperationId: 0x%04X, RequestId: 0x%08X\r\n", ippVersion, operationId, requestId);
+
   if (ippVersion != IPP_SUPPORTED_VERSION) {
     Serial.println("Unsupported IPP version");
-    beginResponse(IPP_ERROR_VERSION_NOT_SUPPORTED, requestId);
+    beginResponse(IPP_SERVER_ERROR_VERSION_NOT_SUPPORTED, requestId, "utf-8");
+    write(IPP_END_OF_ATTRIBUTES_TAG);
+    return false;
+  }
+
+  if (requestId == 0) { //request-id must not be 0 (RFC8011 Section 4.1.2)
+    beginResponse(IPP_CLIENT_ERROR_BAD_REQUEST, requestId, "utf-8");
     write(IPP_END_OF_ATTRIBUTES_TAG);
     return false;
   }
 
   std::map<String, std::set<String>> requestAttributes = parseRequestAttributes();
-  std::set<String> unsupportedAttributes;
+
+  if (requestAttributes.size() == 0) {
+    beginResponse(IPP_CLIENT_ERROR_BAD_REQUEST, requestId, "utf-8");
+    write(IPP_END_OF_ATTRIBUTES_TAG);
+    return false;
+  }
+
   switch (operationId) {
     case IPP_GET_PRINTER_ATTRIBUTES:
       Serial.println("Operation is Get-printer-Attributes");
-      beginResponse(IPP_SUCCESFUL_OK, requestId);
-      write(IPP_OPERATION_ATTRIBUTES_TAG);
-      writeAttribute(IPP_VALUE_TAG_CHARSET, "attributes-charset", *requestAttributes["attributes-charset"].begin());
-      writeAttribute(IPP_VALUE_TAG_NATURAL_LANGUAGE, "attributes-natural-language", "en-us");
-      write(IPP_PRINTER_ATTRIBUTES_TAG);
-      for (String attributeName : requestAttributes["requested-attributes"]) {
-        attribute_value_t attributeValue = getPrinterAttribute(attributeName);
-        if (attributeValue.valueTag == IPP_VALUE_TAG_UNSUPPORTED) {
-          unsupportedAttributes.insert(attributeName);
-        } else {
-          //Serial.printf("Supported attribute: \"%s\"\r\n", attributeName.c_str());
-          writeAttribute(attributeName, attributeValue);
-        }
-      }
-      write(IPP_UNSUPPORTED_ATTRIBUTES_TAG);
-      for (String attributeName : unsupportedAttributes) {
-        //Serial.printf("Unupported attribute: \"%s\"\r\n", attributeName.c_str());
-        writeAttribute(IPP_VALUE_TAG_UNSUPPORTED, attributeName, "unsupported");
-      }
-      write(IPP_END_OF_ATTRIBUTES_TAG);
+      beginResponse(IPP_SUCCESFUL_OK, requestId, *requestAttributes["attributes-charset"].begin());
+      handleGetPrinterAttributesRequest(requestAttributes);
       return false;
+
     case IPP_PRINT_JOB:
       Serial.println("Operation is Print-Job");
+      beginResponse(IPP_SUCCESFUL_OK, requestId, *requestAttributes["attributes-charset"].begin());
+      write(IPP_END_OF_ATTRIBUTES_TAG);
       return true;
+
     default:
-      // TODO: report that operation is not supported
       Serial.println("The requested operation is not supported!");
+      beginResponse(IPP_SERVER_ERROR_OPERATION_NOT_SUPPORTED, requestId, "utf-8");
+      write(IPP_END_OF_ATTRIBUTES_TAG);
       return false;
   }
 }
--- printserver/IppStream.h
+++ printserver/IppStream.h
@@ -8,7 +8,9 @@
 #define IPP_SUPPORTED_VERSION 0x0101
 
 #define IPP_SUCCESFUL_OK 0x0000
-#define IPP_ERROR_VERSION_NOT_SUPPORTED 0x0503
+#define IPP_CLIENT_ERROR_BAD_REQUEST 0x0400
+#define IPP_SERVER_ERROR_OPERATION_NOT_SUPPORTED 0x0501
+#define IPP_SERVER_ERROR_VERSION_NOT_SUPPORTED 0x0503
 
 #define IPP_OPERATION_ATTRIBUTES_TAG 0x01
 #define IPP_JOB_ATTRIBUTES_TAG 0x02
@@ -17,16 +19,26 @@
 #define IPP_UNSUPPORTED_ATTRIBUTES_TAG 0x05
 
 #define IPP_VALUE_TAG_UNSUPPORTED 0x10
+#define IPP_VALUE_TAG_INTEGER 0x21
+#define IPP_VALUE_TAG_BOOLEAN 0x22
+#define IPP_VALUE_TAG_ENUM 0x23
+#define IPP_VALUE_TAG_NAME 0x42
+#define IPP_VALUE_TAG_KEYWORD 0x44
+#define IPP_VALUE_TAG_URI 0x45
 #define IPP_VALUE_TAG_CHARSET 0x47
 #define IPP_VALUE_TAG_NATURAL_LANGUAGE 0x48
+#define IPP_VALUE_TAG_MIME_MEDIA_TYPE 0x49
 
 #define IPP_PRINT_JOB 0x0002
 #define IPP_GET_PRINTER_ATTRIBUTES 0x000B
 
 typedef struct {
   byte valueTag;
-  String value;
+  char* value;
+  int valueLength;
 } attribute_value_t;
+
+#define STRING_ATTRIBUTE_VALUE(tag, str) {tag, str, STRLEN(str)}
 
 class IppStream: public HttpStream {
   public:
@@ -34,7 +46,9 @@ class IppStream: public HttpStream {
     bool parseRequest();
   private:
     std::map<String, std::set<String>> parseRequestAttributes();
-    void beginResponse(uint16_t statusCode, uint32_t requestId);
+    void beginResponse(uint16_t statusCode, uint32_t requestId, String charset);
     void writeAttribute(byte valueTag, String name, String value);
     void writeAttribute(String name, attribute_value_t value);
+
+    void handleGetPrinterAttributesRequest(std::map<String, std::set<String>> requestAttributes);
 };
